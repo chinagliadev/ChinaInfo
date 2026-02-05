436dd3535b9a0f713c7b9546ede0bd7fa07cd4bc
Chinaglia <156460261+chinagliadev@users.noreply.github.com>
2026-02-05 20:43:21 -0300

    feat/listando unidades

--- site-loja/unidades.js
+++ site-loja/unidades.js
@@ -1,33 +1,43 @@
-function fnCarregarDados(){
-    fetch('http://localhost:8000/unidades', {method: 'GET'})
+function fnCarregarDados() {
+    fetch('http://localhost:8000/unidades', { method: 'GET' })
         .then(response => response.json())
-        .then((unidades)=>{
+        .then((unidades) => {
             unidades.forEach(unidade => {
                 fnMontarCardUnidades(unidade)
             });
         })
 }
 
-function fnMontarCardUnidades(unidade){
+function fnMontarCardUnidades(unidade) {
     let cartao = `
         <div class="col-12 col-sm-12 col-md-6 col-lg-4 mb-3">
-                <div class="card shadow">
-                    <img class="imagem-card" src="${unidade.foto}"
-                        class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">${unidade.nome_da_loja}</h5>
-                        <p class="card-text">${unidade.endereco}</p>
-                        <div class="d-flex flex-column mt-1">
-                            <span class="h5 mb-1">Email ${unidade.email}</span>                     
-                            <span class="h5 mb-0">Telefone: ${unidade.telefone}</span>                     
-                        </div>
-                    </div>
-                </div>
+    <div class="card shadow h-100">
+        <img class="imagem-card card-img-top" src="${unidade.foto}" alt="">
+
+        <div class="card-body">
+            <h5 class="card-title">${unidade.nome_da_loja}</h5>
+            <p class="card-text">${unidade.endereco}</p>
+
+            <div class="d-flex flex-column mt-1">
+                <span class="mb-1"><strong>Email:</strong> ${unidade.email}</span>
+                <span class="mb-1"><strong>Telefone:</strong> ${unidade.telefone}</span>
             </div>
+        </div>
+
+        <!-- MAPA -->
+        <div class="map-container">
+            <iframe
+                src="https://maps.google.com/maps?q=${unidade.endereco}&z=14&output=embed"
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
+    </div>
+</div>
     `
     document.querySelector(".lista-unidades").innerHTML += cartao
 }
- 
+
 fnCarregarDados()
 
 
